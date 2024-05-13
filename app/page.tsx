ef0737bebaba9512953c68982508742236d62b8c
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-05-13 13:36:59 +0200

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>Homepage</h1>;
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <br />
+      <Link href="/shopping-card">Shopping Card</Link> <br />
+      <br />
+      <Link href="/33">Product</Link>
+    </div>
+  );
 }
