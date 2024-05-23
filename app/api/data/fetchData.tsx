30c1e6f10d3cff55bdd907e25ffa707cc2739c36
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-05-23 13:45:03 +0200

    .

---
+++ app/api/data/fetchData.tsx
@@ -0,0 +1,9 @@
+//Maybe I will move this function in separate file (folder for utilities)
+//I have to use try-catch function!!
+// async function fetchData(productId: number | string) {
+//   const response = await fetch(
+//     "https://jsonplaceholder.typicode.com/posts/" + productId
+//   );
+//   const product = await response.json();
+//   return product as Product;
+// }
--- app/models/product.ts
+++ app/models/product.ts
@@ -1,4 +1,10 @@
 type Product = {
+  id: number | string;
   title: string;
+  components: [string];
   body: string;
+  tag: string;
+  price: string;
+  favorited: boolean;
+  image?: File;
 };
