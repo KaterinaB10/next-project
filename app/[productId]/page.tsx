1a941ed3ada65848e1c4d1741db3932ff14538c0
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-05-28 14:20:44 +0200

    metadata for dinamic page

--- app/[productId]/page.tsx
+++ app/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import fs from "fs/promises";
 import path from "path";
 import React from "react";
+import { Metadata } from "next";
 import ProductCard from "../components/ProductCard";
 
 interface Product {
@@ -18,6 +19,31 @@ export async function fetchProducts(): Promise<Product[]> {
   const filePath = path.join(process.cwd(), "app", "api", "db.json");
   const fileContents = await fs.readFile(filePath, "utf-8");
   return JSON.parse(fileContents).products;
+}
+
+type MetaDataProps = {
+  params: { productId: number };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata({
+  params,
+  searchParams,
+}: MetaDataProps): Promise<Metadata> {
+  const products = await fetchProducts();
+  const product = products.find((p) => p.id === params.productId);
+
+  if (!product) {
+    return {
+      title: "Product Not Found",
+      description: "The product you are looking for does not exist.",
+    };
+  }
+
+  return {
+    title: product.title,
+    description: product.body,
+  };
 }
 
 export default async function Page({
