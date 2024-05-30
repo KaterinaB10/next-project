dc6efee8e233d67ae3328717cf12701cf3cc5535
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-05-30 14:40:25 +0200

    updated

--- app/components/home-page/hero-section/HeroSection.tsx
+++ app/components/home-page/hero-section/HeroSection.tsx
@@ -1,6 +1,13 @@
+"use client";
 import { openSans } from "../../../fonts/openSans";
-import { Carousel } from "./Carousel";
+import EmblaCarousel from "./EmblaCarousel";
 import styles from "./heroSection.module.css";
+import { EmblaOptionsType } from "embla-carousel";
+import "./embla.css";
+
+const OPTIONS: EmblaOptionsType = { loop: true };
+const SLIDE_COUNT = 3;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
 
 export function Hero() {
   return (
@@ -9,7 +16,7 @@ export function Hero() {
         <h1 className={openSans.className}>Choose your favorite taste</h1>
         <h3>using our ordering platform.</h3>
       </div>
-      <Carousel />
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
     </section>
   );
 }
