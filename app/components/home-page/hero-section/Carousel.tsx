0e6c130716cac8e975dd3b5919bddd2f6f43b213
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-06-03 14:16:25 +0200

    .

--- app/components/home-page/hero-section/Carousel.tsx
+++ app/components/home-page/hero-section/Carousel.tsx
@@ -1,28 +1,7 @@
-import Image from "next/image";
-
 export function Carousel() {
   return (
     <div className="carouselContainer">
-      <div>
-        <Image
-          src="/images/chocolate-coffee.png"
-          alt=""
-          width={250}
-          height={250}
-        />
-        <Image
-          src="/images/chocolate-coffee.png"
-          alt=""
-          width={250}
-          height={250}
-        />
-        <Image
-          src="/images/chocolate-coffee.png"
-          alt=""
-          width={250}
-          height={250}
-        />
-      </div>
+      <div></div>
     </div>
   );
 }
