e86f0c89ee35314e2c69f6e22b6bc9dc7e8d352c
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-06-06 10:26:36 +0200

    .

--- app/components/home-page/hero-section/Carousel.tsx
+++ app/components/home-page/hero-section/Carousel.tsx
@@ -1,7 +1,47 @@
-export function Carousel() {
+import React, { useCallback } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import { fetchProduct } from "./fetchProduct";
+import Image from "next/image";
+
+export async function EmblaCarousel = () => {
+  const products: Product[] = await fetchProduct();
+  const favoritedProducts = products.filter((product) => product.favorited);
+
+  const [emblaRef, emblaApi] = useEmblaCarousel();
+
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
   return (
-    <div className="carouselContainer">
-      <div></div>
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {favoritedProducts.map((product: Product) => (
+            <div key={product.id}>
+              <Image
+                src={product.image}
+                alt={product.body}
+                width={50}
+                height={50}
+              />
+            </div>
+          ))}
+          <div className="embla__slide">Slide 1</div>
+          <div className="embla__slide">Slide 2</div>
+          <div className="embla__slide">Slide 3</div>
+        </div>
+      </div>
+      <button className="embla__prev" onClick={scrollPrev}>
+        Prev
+      </button>
+      <button className="embla__next" onClick={scrollNext}>
+        Next
+      </button>
     </div>
   );
-}
+};
--- app/components/home-page/hero-section/HeroSection.tsx
+++ app/components/home-page/hero-section/HeroSection.tsx
@@ -1,13 +1,7 @@
 "use client";
 import { openSans } from "../../../fonts/openSans";
-import EmblaCarousel from "./EmblaCarousel";
 import styles from "./heroSection.module.css";
-import { EmblaOptionsType } from "embla-carousel";
-import "./embla.css";
-
-const OPTIONS: EmblaOptionsType = { loop: true };
-const SLIDE_COUNT = 3;
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+import { EmblaCarousel } from "./Carousel";
 
 export function Hero() {
   return (
@@ -16,7 +10,7 @@ export function Hero() {
         <h1 className={openSans.className}>Choose your favorite taste</h1>
         <h3>using our ordering platform.</h3>
       </div>
-      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      <EmblaCarousel />
     </section>
   );
 }
